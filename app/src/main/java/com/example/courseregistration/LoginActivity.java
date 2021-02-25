385aac5ef565fb65591aed10e94799e44690096f
jjddww <ekdns29@naver.com>
2021-02-25 17:18:24 +0900

    3. 안내 메세지 수정

--- app/src/main/java/com/example/courseregistration/LoginActivity.java
+++ app/src/main/java/com/example/courseregistration/LoginActivity.java
@@ -64,7 +64,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(getApplicationContext(),"존재하지 않는 학번입니다.", Toast.LENGTH_SHORT).show();
 
                             else if (access.equals("0"))
-                                Toast.makeText(getApplicationContext(),"비밆번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(),"비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
 
                             else{
                                 //로그인 성공
